a1467d3bb2309194945d970cd6ea11744cf1e9b8
iwaki1990 <low.son.wing@gmail.com>
2021-06-10 20:45:42 +0900

    4 commit

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -9,7 +9,26 @@ class PersonController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
+        $items = DB::table('people')->orderBy('age', 'asc')->get();
         return view('index', ['items' => $items]);
+    }
+    public function show(Request $request)
+    {
+        $page = $request->page;
+        $items = DB::table('people')->offset($page * 3)->limit(3)->get();
+        return view('show', ['items' => $items]);
+    }
+    public function add(Request $request)
+    {
+        return view('add');
+    }
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'age' => $request->age
+        ];
+        DB::table('people')->insert($param);
+        return redirect('/');
     }
 }
