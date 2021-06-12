c92ccde6419f52145abac473771bab8bad65645e
iwaki1990 <low.son.wing@gmail.com>
2021-06-12 17:27:51 +0900

    8 commit

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Models\Person;
 
 class PersonController extends Controller
 {
     public function index(Request $request){
-      $items = DB::table('people')->simplePaginate(5);
-      return view('index', ['items'=>$items]);
+      $sort = $request->sort;
+      $items = Person::orderBy($sort,'asc')->simplePaginate();
+      $param = ['items'=>$items, 'sort'=>$sort];
+      return view('index',$param);
     }
 }
